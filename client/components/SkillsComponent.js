9290615815e19857c51f19825010427e9f814a22
Kamil <kaqil91@gmail.com>
2017-11-01 00:24:09 -0700

    styled skills component a little

--- client/components/SkillsComponent.js
+++ client/components/SkillsComponent.js
@@ -3,9 +3,83 @@ import React from 'react';
 class SkillsComponent extends React.Component {
     render() {
         return (
-            <div id='skillsComponent'>
-                skills these are my skills 
+            <div id='skillsComponent' class='componentHeader'>
+                <span id='skillsHeader'>Skills</span>
+                <div id='skillsIconContainer'>
+                    {/* angularIcon */}
+                    <div id='angularIconContainer'>
+                        <img src="./images/skillsSvg/angularjs.svg" alt=""/>
+                    </div>
+                    {/* gitHubIcon */}
+                    <div id='gitHubIconContainer'>
+                        <img src="./images/skillsSvg/github.svg" alt="" />
+                    </div>
+                    {/* angularIcon */}
+                    <div id='angularIconContainer'>
+                        <img src="./images/skillsSvg/angularjs.svg" alt="" />
+                    </div>
+                    {/* angularIcon */}
+                    <div id='angularIconContainer'>
+                        <img src="./images/skillsSvg/angularjs.svg" alt="" />
+                    </div>
+                    {/* angularIcon */}
+                    <div id='angularIconContainer'>
+                        <img src="./images/skillsSvg/angularjs.svg" alt="" />
+                    </div>
+                    {/* angularIcon */}
+                    <div id='angularIconContainer'>
+                        <img src="./images/skillsSvg/angularjs.svg" alt="" />
+                    </div>
+                    {/* angularIcon */}
+                    <div id='angularIconContainer'>
+                        <img src="./images/skillsSvg/angularjs.svg" alt="" />
+                    </div>
+                    {/* angularIcon */}
+                    <div id='angularIconContainer'>
+                        <img src="./images/skillsSvg/angularjs.svg" alt="" />
+                    </div>
+                    {/* angularIcon */}
+                    <div id='angularIconContainer'>
+                        <img src="./images/skillsSvg/angularjs.svg" alt="" />
+                    </div>
+                    {/* angularIcon */}
+                    <div id='angularIconContainer'>
+                        <img src="./images/skillsSvg/angularjs.svg" alt="" />
+                    </div>
+                    {/* angularIcon */}
+                    <div id='angularIconContainer'>
+                        <img src="./images/skillsSvg/angularjs.svg" alt="" />
+                    </div>
+                    {/* angularIcon */}
+                    <div id='angularIconContainer'>
+                        <img src="./images/skillsSvg/angularjs.svg" alt="" />
+                    </div>
+                    {/* angularIcon */}
+                    <div id='angularIconContainer'>
+                        <img src="./images/skillsSvg/angularjs.svg" alt="" />
+                    </div>
+                    {/* angularIcon */}
+                    <div id='angularIconContainer'>
+                        <img src="./images/skillsSvg/angularjs.svg" alt="" />
+                    </div>
+                    {/* angularIcon */}
+                    <div id='angularIconContainer'>
+                        <img src="./images/skillsSvg/angularjs.svg" alt="" />
+                    </div>
+                    {/* angularIcon */}
+                    <div id='angularIconContainer'>
+                        <img src="./images/skillsSvg/angularjs.svg" alt="" />
+                    </div>
+                    {/* angularIcon */}
+                    <div id='angularIconContainer'>
+                        <img src="./images/skillsSvg/angularjs.svg" alt="" />
+                    </div>
+                    {/* angularIcon */}
+                    <div id='angularIconContainer'>
+                        <img src="./images/skillsSvg/angularjs.svg" alt="" />
+                    </div>
                 </div>
+            </div>
         )
     }
 }
