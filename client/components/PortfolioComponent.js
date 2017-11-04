5cca5f09bf7bcb3865ad4a6f5106325c0fcc3e1c
Kamil <kaqil91@gmail.com>
2017-11-03 20:05:34 -0700

    updating style changes

--- client/components/PortfolioComponent.js
+++ client/components/PortfolioComponent.js
@@ -3,22 +3,22 @@ import React from 'react';
 class PortfolioComponet extends React.Component {
     render() {
         return (
-            <div id='portfolioComponent' class='componentHeader'>
+            <div id='portfolioComponent' className='componentHeader'>
                 <span id='portfolioComponentHeader'>Portfolio</span>
                 {/* gallery container */}
                 <div id='portfolioGalleryContainer'>
                     {/* wheel of fortune gif */}
-                    <img src="./images/gifs/hangManGame.gif" class='galleryGif' id='wheelOfFortuneGif' alt=""/>
+                    <img src="./images/gifs/hangManGame.gif" className='galleryGif' id='wheelOfFortuneGif' alt=""/>
                     {/* bamazon gif */}
-                    <img src="./images/gifs/bamazonGif.gif" id='bamazonGif' class='galleryGif' alt="" />
+                    <img src="./images/gifs/bamazonGif.gif" id='bamazonGif' className='galleryGif' alt="" />
                     {/* giphy gif */}
-                    <img src="./images/gifs/giphyAppGif.gif" id='giphyGif' class='galleryGif' alt="" />
+                    <img src="./images/gifs/giphyAppGif.gif" id='giphyGif' className='galleryGif' alt="" />
                     {/* noir gif */}
-                    <img src="./images/gifs/noirGif.gif" id='noirGif' class='galleryGif' alt="" />
+                    <img src="./images/gifs/noirGif.gif" id='noirGif' className='galleryGif' alt="" />
                     {/* rpg Gif */}
-                    <img src="./images/gifs/rpgGif.gif" id='rpgGif' class='galleryGif' alt="" />
+                    <img src="./images/gifs/rpgGif.gif" id='rpgGif' className='galleryGif' alt="" />
                     {/* trivia game */}
-                    <img src="./images/gifs/triviaGameGif.gif" id='triviaGif' class='galleryGif' alt="" />      
+                    <img src="./images/gifs/triviaGameGif.gif" id='triviaGif' className='galleryGif' alt="" />      
                 </div>
             </div>
         )
--- client/components/SkillsComponent.js
+++ client/components/SkillsComponent.js
@@ -3,79 +3,79 @@ import React from 'react';
 class SkillsComponent extends React.Component {
     render() {
         return (
-            <div id='skillsComponent' class='componentHeader'>
+            <div id='skillsComponent' className='componentHeader'>
                 <span id='skillsHeader'>Skills</span>
                 <div id='skillsIconContainer'>
                     {/* angularIcon */}
-                    <div class='skillsSvg' id='angularIconContainer'>
+                    <div className='skillsSvg' id='angularIconContainer'>
                         <img src="./images/skillsSvg/angularjs.svg" alt=""/>
                     </div>
                     {/* gitHubIcon */}
-                    <div class='skillsSvg' id='gitHubIconContainer'>
+                    <div className='skillsSvg' id='gitHubIconContainer'>
                         <img src="./images/skillsSvg/github.svg" alt="" />
                     </div>
                     {/* handlebarsIcon */}
-                    <div class='skillsSvg' id='handlebarsIconContainer'>
+                    <div className='skillsSvg' id='handlebarsIconContainer'>
                         <img src="./images/skillsSvg/handlebarsIcon.svg" alt="" />
                     </div>
                     {/* herokuIcon */}
-                    <div class='skillsSvg' id='herokuIconContainer'>
+                    <div className='skillsSvg' id='herokuIconContainer'>
                         <img src="./images/skillsSvg/herokuIcon.svg" alt="" />
                     </div>
                     {/* babelIcon */}
-                    <div class='skillsSvg' id='babelIconContainer'>
+                    <div className='skillsSvg' id='babelIconContainer'>
                         <img src="./images/skillsSvg/babelIcon.svg" alt="" />
                     </div>
                     {/* expressIcon */}
-                    <div class='skillsSvg' id='expressIconContainer'>
+                    <div className='skillsSvg' id='expressIconContainer'>
                         <img src="./images/skillsSvg/expressIcon.svg" alt="" />
                     </div>
                     {/* javascriptIcon */}
-                    <div class='skillsSvg' id='javascriptIconContainer'>
+                    <div className='skillsSvg' id='javascriptIconContainer'>
                         <img src="./images/skillsSvg/javascriptIcon.svg" alt="" />
                     </div>
                     {/* html5Icon */}
-                    <div class='skillsSvg' id='html5IconContainer'>
+                    <div className='skillsSvg' id='html5IconContainer'>
                         <img src="./images/skillsSvg/html5Icon.svg" alt="" />
                     </div>
                     {/* jQueryIcon */}
-                    <div class='skillsSvg' id='jQueryIconContainer'>
+                    <div className='skillsSvg' id='jQueryIconContainer'>
                         <img src="./images/skillsSvg/jqueryIcon.svg" alt="" />
                     </div>
                     {/* mongodbIcon */}
-                    <div class='skillsSvg' id='mongodbIconContainer'>
+                    <div className='skillsSvg' id='mongodbIconContainer'>
                         <img src="./images/skillsSvg/mongodbIcon.svg" alt="" />
                     </div>
                     {/* mysqlIcon */}
-                    <div class='skillsSvg' id='mysqlIconContainer'>
+                    <div className='skillsSvg' id='mysqlIconContainer'>
                         <img src="./images/skillsSvg/mysqlIcon.svg" alt="" />
                     </div>
                     {/* nodeIcon */}
-                    <div class='skillsSvg' id='nodeIconContainer'>
+                    <div className='skillsSvg' id='nodeIconContainer'>
                         <img src="./images/skillsSvg/nodejsIcon.svg" alt="" />
                     </div>
                     {/* npmIcon */}
-                    <div class='skillsSvg' id='npmIconContainer'>
+                    <div className='skillsSvg' id='npmIconContainer'>
                         <img src="./images/skillsSvg/npmIcon.svg" alt="" />
                     </div>
                     {/* reactIcon */}
-                    <div class='skillsSvg' id='reactIconContainer'>
+                    <div className='skillsSvg' id='reactIconContainer'>
                         <img src="./images/skillsSvg/reactIcon.svg" alt="" />
                     </div>
                     {/* sassIcon */}
-                    <div class='skillsSvg' id='sassIconContainer'>
+                    <div className='skillsSvg' id='sassIconContainer'>
                         <img src="./images/skillsSvg/sassIcon.svg" alt="" />
                     </div>
                     {/* webpackIcon */}
-                    <div class='skillsSvg' id='webpackIconContainer'>
+                    <div className='skillsSvg' id='webpackIconContainer'>
                         <img src="./images/skillsSvg/webpackIcon.svg" alt="" />
                     </div>
                     {/* wordpressIcon */}
-                    <div class='skillsSvg' id='wordpressIconContainer'>
+                    <div className='skillsSvg' id='wordpressIconContainer'>
                         <img src="./images/skillsSvg/wordpressIcon.svg" alt="" />
                     </div>
                     {/* bootstrapIcon */}
-                    <div class='skillsSvg' id='bootstrapIconContainer'>
+                    <div className='skillsSvg' id='bootstrapIconContainer'>
                         <img src="./images/skillsSvg/bootstrapIcon.svg" alt="" />
                     </div>
                 </div>
