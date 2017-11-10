5d163979360753dc0bf844b1f7805052238ef83d
Kamil <kaqil91@gmail.com>
2017-11-09 17:45:03 -0800

    have portfolio images in their own containers as well as headings

--- client/components/PortfolioComponent.js
+++ client/components/PortfolioComponent.js
@@ -8,19 +8,35 @@ class PortfolioComponet extends React.Component {
                 {/* gallery container */}
                 <div id='portfolioGalleryContainer'>
                     {/* wheel of fortune gif hangman */}
-                    <div id='projectOneContainer'>
+                    <div id='projectOneContainer' className='portfolioProjectContainer'>
+                        <span className='portfolioProjectHeader'>Wheel of Fortune Hangman Game</span>
                         <img src="./images/gifs/hangManGame.gif" className='galleryGif' id='wheelOfFortuneGif' alt="" />
                     </div>
                     {/* bamazon gif */}
-                    <img src="./images/gifs/bamazonGif.gif" id='bamazonGif' className='galleryGif' alt="" />
+                    <div id='projectTwoContainer' className='portfolioProjectContainer'>
+                        <span className='portfolioProjectHeader'>Inventory Mangagement</span>
+                        <img src="./images/gifs/bamazonGif.gif" id='bamazonGif' className='galleryGif' alt="" />
+                    </div>
                     {/* giphy gif */}
-                    <img src="./images/gifs/giphyAppGif.gif" id='giphyGif' className='galleryGif' alt="" />
+                    <div id='projectThreeContainer' className='portfolioProjectContainer'>
+                        <span className='portfolioProjectHeader'>Gif Gallery</span>
+                        <img src="./images/gifs/giphyAppGif.gif" id='giphyGif' className='galleryGif' alt="" />
+                    </div>
                     {/* noir gif */}
-                    <img src="./images/gifs/noirGif.gif" id='noirGif' className='galleryGif' alt="" />
+                    <div id='projectFourContainer' className='portfolioProjectContainer'>
+                        <span className='portfolioProjectHeader'>Noir Clothing Store</span>
+                        <img src="./images/gifs/noirGif.gif" id='noirGif' className='galleryGif' alt="" />
+                    </div>
                     {/* rpg Gif */}
-                    <img src="./images/gifs/rpgGif.gif" id='rpgGif' className='galleryGif' alt="" />
+                    <div id='projectFiveContainer' className='portfolioProjectContainer'>
+                        <span className='portfolioProjectHeader'>Simple Rpg</span>
+                        <img src="./images/gifs/rpgGif.gif" id='rpgGif' className='galleryGif' alt="" />
+                    </div>
                     {/* trivia game */}
-                    <img src="./images/gifs/triviaGameGif.gif" id='triviaGif' className='galleryGif' alt="" />      
+                    <div id='projectSixContainer' className='portfolioProjectContainer'>
+                        <span className='portfolioProjectHeader'>Trivia Game</span>
+                        <img src="./images/gifs/triviaGameGif.gif" id='triviaGif' className='galleryGif' alt="" />      
+                    </div>
                 </div> 
             </div>
         )
